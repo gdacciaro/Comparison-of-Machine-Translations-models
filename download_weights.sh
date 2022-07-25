2b8dbbf51bb8205be3038c2526d584efa4a71eaf
Acciaro Gennaro Daniele <acciarogennaro@gmail.com>
2022-07-25 16:03:39 +0200

    Avanzamenti

--- download_weights.sh
+++ download_weights.sh
@@ -1,5 +1,5 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1fBe7syWG0kBrd1zsX6u7nJwtCcLj67QZ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1fBe7syWG0kBrd1zsX6u7nJwtCcLj67QZ" -O weights.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1fBe7syWG0kBrd1zsX6u7nJwtCcLj67QZ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Boo9w_mLJeqCilBCZNr5VOGGrbfc202H" -O weights.zip && rm -rf /tmp/cookies.txt
 
-unzip weights.zip
+unzip models.zip
 
-rm weights.zip
+rm models.zip
